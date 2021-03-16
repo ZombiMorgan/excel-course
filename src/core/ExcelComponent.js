cdf49f7d8d9b508673cb68551ac52fae9930218a
Zombi_Morgan <portvein666@list.ru>
2021-03-16 12:51:20 +0300

    Finish base framework functional

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,23 @@
+import {DOMListener} from '@core/DOMListener';
+
+export class ExcelComponent extends DOMListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+    this.name = options.name || '';
+  }
+  /**
+   * Возвращает шаблон компонента
+   * @return {string}
+   */
+  toHTML() {
+    return '';
+  }
+
+  init() {
+    this.initDOMListeners();
+  }
+
+  destroy() {
+    this.removeDOMListeners();
+  }
+}
